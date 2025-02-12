7c037bf424b7f841bdf6de140675456977530467
zhuisu <kuaihen@Gmail.com>
2025-02-13 00:52:12 +0800

    2025年2月13日00:51:38

--- 自动追溯1.1.py
+++ 自动追溯1.1.py
@@ -1,3 +1,6 @@
+import time
+from os import times
+
 from selenium import webdriver  # 用于操作浏览器
 from selenium.webdriver.chrome.options import Options  # 设置谷歌浏览器
 from selenium.webdriver.chrome.service import Service  # 管理谷歌驱动
@@ -60,6 +63,54 @@ def login_process(driver):
                '13285909090')
     input_text(driver, '/html/body/div[2]/div[3]/div[2]/div[2]/div[2]/div[2]/div[2]/form/div/div[4]/div[2]/input',
                'wl123321')
+    # 点击登入
+    # 等待3秒手动输入验证码
+    time.sleep(8)
+    click_element(driver,'//*[@id="submit1"]')
+    # 进入销售追溯
+    click_element(driver,'//*[@id="jquery-accordion-menu"]/ul/li[2]/a')
+    # 进入进货登记
+    click_element(driver,'//*[@id="jquery-accordion-menu"]/ul/li[2]/ul/li[2]/ul/li[1]/a')
+    # 新增
+    click_element(driver,'/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/div/div[1]/div[2]/div[1]/span[1]/span/span/span[1]')
+    # 从备案库选择产品
+    click_element(driver,'/html/body/div[9]/div[2]/div[1]/div[1]/div[4]/div/div[2]/span[2]')
+    # 输入产品名称
+    time.sleep(1)
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[1]/form/table/tbody/tr[1]/td[2]/div/div[2]/input','大白菜')
+    time.sleep(1)
+    # 设置生产厂商
+    input_text(driver,
+               '/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[1]/form/table/tbody/tr[1]/td[4]/div/div[2]/input',
+               '新罗区谢贤福蔬菜摊')
+    time.sleep(1)
+    # 点击查询
+    click_element(driver,
+                  '/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[1]/form/table/tbody/tr[2]/td[3]/span[1]/span')
+    time.sleep(2)
+    # 输入日期
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[2]/div/div[3]/div[2]/div/table/tbody/tr/td[6]/div/div/div[3]/input[1]','2025-02-12')
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[2]/div/div[3]/div[2]/div/table/tbody/tr/td[7]/div/div/div[2]/input','20250212')
+    # 输入数量
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[2]/div/div[3]/div[2]/div/table/tbody/tr/td[8]/div/div/div[3]/input[1]','10')
+    # 点击添加
+    click_element(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[2]/div/div[2]/div/div[3]/div[2]/div/table/tbody/tr/td[12]/a')
+
+    # 进入进货台账
+    click_element(driver, '/html/body/div[9]/div[2]/div[1]/div[1]/div[4]/div/div[1]/span[2]')
+    # 设置供货方
+    click_element(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[1]/div/div/div[2]/form/table/tbody/tr[1]/td[3]/div/div[2]')
+    input_text(driver,'/html/body/div[15]/div/div[1]/div[2]/input','新罗区谢贤福蔬菜摊')
+    click_element(driver, '/html/body/div[15]/div/span[1]/span/span/span[3]')
+    time.sleep(2)
+    click_element(driver, '/html/body/div[15]/div/div[5]/div[3]/div[2]/div/table/tbody/tr/td[1]')
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[1]/div/div/div[2]/form/table/tbody/tr[4]/td/div[1]/div[2]/input','福建万鹭贸易有限公司')
+    time.sleep(1)
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[1]/div/div/div[2]/form/table/tbody/tr[4]/td/div[2]/div[2]/input','闽FZN138')
+    time.sleep(1)
+    input_text(driver,'/html/body/div[9]/div[2]/div[1]/div[3]/div[1]/div/div/div[2]/form/table/tbody/tr[4]/td/div[3]/div[2]/input','13950826868')
+    click_element(driver,'/html/body/div[9]/div[2]/div[2]/span[2]/span/span/span[3]')
+
 
 
 if __name__ == "__main__":
